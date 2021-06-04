f8550cba3cf35506a79ce67e276044ecf1f78353
Tommy Williams <tmmytwllms@gmail.com>
2021-06-04 13:59:43 -0400

    Fixed data types on tables, fixed primary key for titles

---
+++ queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM salaries;
--- schema.sql
+++ schema.sql
@@ -19,8 +19,8 @@ CREATE TABLE employees(
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
-	from_date INT NOT NULL,
-	to_date INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no,dept_no)
@@ -41,7 +41,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no,title,from_date)
 );
 
 CREATE TABLE dept_emp(
