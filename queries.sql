b72175e0765734d9bb96cb00d7236aae5b0450c2
Tommy Williams <tmmytwllms@gmail.com>
2021-06-04 15:20:54 -0400

    queried the employees data set to create the retirement info csv containing employees who will soon be retiring. Queries included in sql doc

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+--View the newly created tables after importing the CSV data
 SELECT * FROM departments;
 
 SELECT * FROM employees;
@@ -7,3 +8,56 @@ SELECT * FROM dept_manager;
 SELECT * FROM dept_emp;
 
 SELECT * FROM salaries;
+
+--Search for all employees born between 1952 and 1955
+SELECT first_name, last_name FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+--Display a count of these employees
+SELECT COUNT(*) FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Search for all employees born in 1952
+SELECT first_name, last_name FROM EMPLOYEES
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
+--Display a count of these employees
+SELECT COUNT(*) FROM EMPLOYEES
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'
+
+--Search for all employees born in 1953
+SELECT first_name, last_name FROM EMPLOYEES
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+--Display a count of these employees
+SELECT COUNT(*) FROM EMPLOYEES
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'
+
+--Search for all employees born in 1954
+SELECT first_name, last_name FROM EMPLOYEES
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+--Display a count of these employees
+SELECT COUNT(*) FROM EMPLOYEES
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+--Search for all employees born in 1955
+SELECT first_name, last_name FROM EMPLOYEES
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+--Display a count of these employees
+SELECT COUNT(*) FROM EMPLOYEES
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+--Retirement eligibility
+SELECT first_name, last_name FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Gather retirement data and store it in a new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
